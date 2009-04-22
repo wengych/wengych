89c1eb778b009a67a96ceb9101dabef543a21c3e
wengych <wengych@debian.(none)>
2009-04-22 17:31:04 +0800

    增加列表框 增加发送按钮

--- ysClient.gtkmm/src/MainWindow.cpp
+++ ysClient.gtkmm/src/MainWindow.cpp
@@ -6,30 +6,57 @@
  */
 
 #include "MainWindow.h"
-// #include <ysdef.h>
+#include <ysdef.h>
 
 MainWindow::MainWindow() :
-    m_inputFrame("输入"), m_outputFrame("输出"), m_vBox(false, 5), m_hBox(false, 5)
+    m_inputFrame("输入"), m_outputFrame("输出"), m_serviceList(1),
+            m_btnSendRequest("发送请求"), m_vBox(false, 5), m_hBox1(false, 5)
 {
     using namespace std;
     this->set_title("ysClient");
     this->set_border_width(30);
 
-    m_hBox.pack_start(m_inputFrame, Gtk::PACK_SHRINK);
+    m_hBox1.pack_start(m_inputFrame, Gtk::PACK_SHRINK);
 
-    m_inputFrame.add_item(new Gtk::Label("__DICT_IN"), new Gtk::Entry());
-    m_inputFrame.add_item(new Gtk::Label("__DICT_IN2"), new Gtk::Entry());
+    m_hBox1.pack_start(m_outputFrame, Gtk::PACK_SHRINK);
 
-    m_hBox.pack_start(m_outputFrame, Gtk::PACK_SHRINK);
+    m_serviceList.set_size_request(100, 100);
+    m_serviceList.set_column_title(0, "Service List");
+    m_serviceList.append_text("First column");
+    m_serviceList.append_text("Second column");
 
-    m_vBox.pack_start(m_hBox);
+    m_serviceList.add_events();
+    m_serviceList.signal_columns_changed().connect(sigc::mem_fun(*this,
+            &MainWindow::on_columns_changed));
+    m_hBox1.pack_start(m_serviceList, Gtk::PACK_SHRINK);
+
+    //// ================
+    m_hBox2.pack_start(m_btnSendRequest);
+
+    //// ================
+    m_vBox.pack_start(m_hBox1);
+    m_vBox.pack_start(m_hBox2);
 
     this->get_vbox()->pack_start(m_vBox);
 
     show_all();
+    std::cout << "show_all()" << std::endl;
 }
 
+bool MainWindow::on_service_list_press(GdkEventButton* event)
+{
+    std::cout << "on_service_list_press" << std::endl;
+
+    return true;
+}
+
+void MainWindow::on_columns_changed()
+{
+    std::cout << "on_columns_changed()" << std::endl;
+
+    m_inputFrame.add_item(new Gtk::Label("__DICT_IN"), new Gtk::Entry());
+    m_inputFrame.add_item(new Gtk::Label("__DICT_IN2"), new Gtk::Entry());
+}
 MainWindow::~MainWindow()
 {
-    // TODO Auto-generated destructor stub
 }
--- ysClient.gtkmm/src/MainWindow.h
+++ ysClient.gtkmm/src/MainWindow.h
@@ -8,7 +8,9 @@
 #include <gtkmm/dialog.h>
 #include <gtkmm/label.h>
 #include <gtkmm/entry.h>
+#include <gtkmm/button.h>
 #include <gtkmm/textview.h>
+#include <gtkmm/listviewtext.h>
 #include <gtkmm/box.h>
 
 #include <iostream>
@@ -24,12 +26,19 @@ public:
     MainWindow();
     virtual ~MainWindow();
 
+    bool on_service_list_press(GdkEventButton*);
+    void on_columns_changed();
+
 protected:
     YsFrame m_inputFrame;
     YsFrame m_outputFrame;
+    Gtk::ListViewText m_serviceList;
+
+    Gtk::Button m_btnSendRequest;
 
     Gtk::VBox m_vBox;
-    Gtk::HBox m_hBox;
+    Gtk::HBox m_hBox1;
+    Gtk::HButtonBox m_hBox2;
 };
 
 #endif /* MAINWINDOW_H_ */
